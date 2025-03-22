3feeff437c3a28fea0fbcb3d9660a75bbc057d71
naqeebnadir001 <naqeeb.nadir001@gmail.com>
2025-03-22 17:45:17 +0500

    Auditing File Audit
    File Audit has been done and committed

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import argparse
 from audits import (
-    user_audit
+    user_audit,
+    file_audit  # Import the file audit module
 )
 
 def main():
     parser = argparse.ArgumentParser(description="Windows Auditing Tool for System Security & Compliance")
     parser.add_argument("--all", action="store_true", help="Run all audits")
     parser.add_argument("--users", action="store_true", help="Audit user accounts")
+    parser.add_argument("--files", action="store_true", help="Audit files (Sensitive, Critical, Setuid)")
+    
     args = parser.parse_args()
 
     if args.all or args.users:
         print("\n[+] Running User Audit...")
         user_audit.run_audit()
+
+    if args.all or args.files:
+        print("\n[+] Running File Audit...")
+        file_audit.audit()  # Run the file audit
 
 if __name__ == "__main__":
     main()
